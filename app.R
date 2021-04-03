cbc215a8e5e5fb45d2d57ef4775efae505544d05
diarmuid_lloyd <diarmuid.lloyd@gmail.com>
2021-04-03 16:57:03 +0100

    Read in level4 data and carry out some cleaning
    I added hte level 4 spreadheet tobe read in and tidies up a bit. Not very pretty, but seems to work

--- app.R
+++ app.R
@@ -7,45 +7,97 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)  # Required to run any Shiny app
+library(shiny)
+library(shinydashboard)# Required to run any Shiny app
 library(ggplot2)  # For creating pretty plots
 library(dplyr)  # For filtering and manipulating data
-library(agridat)  # The package where the data comes from
+library(formattable) # For pretty table formatting
+library(sunburstR)
 
 # Loading data ----
 load("data/app_data.rdata")
 
 # ui.R ----
-ui <- fluidPage(
-  theme = bslib::bs_theme(bootswatch = "flatly"),
+ui <- dashboardPage(
   
-  titlePanel("2021-22 Budget"),
+  dashboardHeader(title = "Demo Budget Dashboard with minimal formatting", titleWidth = 450),
   
-  sidebarLayout(
-    sidebarPanel(
-      h1("Portfolio selection") ,
-      selectInput(
-        "portfolio",
-        label = "Choose a portfolio to filter the table",
-        choices = c("All", unique(df_budget$Portfolio)),
-        selected = "All"
-      ),
+  
+  dashboardSidebar( 
+    sidebarMenu(
+      menuItem("2021-22 Budget", tabName = "2021-22_budget", icon = icon("coins")),
+      menuItem("Outturn", icon = icon("receipt"), tabName = "widgets",
+               badgeLabel = "new", badgeColor = "green")
+    )
+   
+  ) ,
+  dashboardBody(
+    fluidRow(
+      column(width = 3,
+    h1("Portfolio selection") ,
+    selectInput(
+      "portfolio",
+      label = "Select a portfolio",
+      choices = c("All", unique(df_budget$Portfolio)),
+      selected = "All"
     ),
-    mainPanel(DT::dataTableOutput("selected_port"))
-  )
+    
+    selectInput(
+      "year_select",
+      label = "Select a year",
+      choices = c("2018-19", "2019-20", "2020-21", "2021-22"),
+      selected = "2021-22"
+    )
+    ),
+    
+    column(width = 9,
+    h4("Example: A table showing key Level 3 data for the selected Portfolio. The table is scrollable and sortable."),
+    
+    DT::dataTableOutput("selected_port"))
+    
+    
+  
 )
-# server.R ----
+))
+
 server <- function(input, output) {
   output$selected_port <- DT::renderDataTable({
+    
     if (input$portfolio != "All") {
       df_budget <-  df_budget %>% filter(Portfolio == input$portfolio)
     }
     
     #plot table
-    df_budget %>% select(Portfolio, `Level 3` = level3, everything()) %>%
+    #First we make some edits to volumns names and rounding
+   df_budget <-  df_budget %>% select(Portfolio, `Level 3` = level3, everything()) %>%
       mutate(`Cash terms change - %` = 100 * `Cash terms change - %`) %>%
-      mutate_if(is.numeric , ~ round(., 2))     %>%
-      DT::datatable(rownames = FALSE)
+      mutate_if(is.numeric , ~ round(., 2))  
+   
+   #Then we use formattable to add some nice formatting
+   df_budget <- 
+     df_budget %>% 
+     mutate(dummy_pc_change = `Cash terms change - %` ) %>% 
+     formattable(list(dummy_pc_change = formatter("span", style = ~style  (
+       color =
+         ifelse(`Cash terms change - %` < 0 ,'purple',ifelse(`Cash terms change - %` > 0,'orange','darkgray'))
+       
+         ),
+       
+       ~icontext(ifelse(`Cash terms change - %` > 0, "arrow-up", ifelse(`Cash terms change - %` < 0,"arrow-down","minus")))
+     )))
+   
+   
+   # Finally we convert to a datatable and display
+   df_budget %>% formattable::as.datatable(
+     rownames = FALSE,
+     options = list(
+       pageLength = 5,
+       paging = FALSE,
+       scrollInfinite = TRUE,
+       scrollY = '300px',
+       scrollCollapse = TRUE
+     ) )
+    
     
   })
   
--- process_level4_sheets.R
+++ process_level4_sheets.R
@@ -14,7 +14,7 @@
 ##
 ## Notes:
 ##   https://parliament.scot/chamber-and-committees/research-prepared-for-parliament/financial-scrutiny?qry=budget%202021
-## is a good place to get data
+##  for data
 ##
 ## ---------------------------
 
@@ -55,6 +55,61 @@ totals <- df_budget %>%
   group_by(Portfolio) %>%
   summarise(budget21 = sum(`2021-22 - £m (cash)`))
 
+
+
+#These are the level 4 lines, nested with Level 1,2 and 3 lines
+# Get list of all sheet names - the data are in all but contents sheet, so pick last 11.
+sheets <- readODS::list_ods_sheets("data/Budget Level 4 2021 to 2022.ods") %>% 
+  tail(11)
+
+
+level4data = purrr::map_df(
+  sheets,
+.f = function(x)
+    readODS::read_ods(
+      "data/Budget Level 4 2021 to 2022.ods",
+      sheet = x,col_names = FALSE
+    )
+)
+
+variable_names <-  c("tmp",
+                   "portfolio",
+                   "level2",
+                   "level3",
+                   "level4",
+                   "level4_alt",
+                   "fiscal_resource_2020",
+                   "non_cash_2020"	,
+                   "capital_2020"	,
+                   "financial_transactions_2020",
+                   "UK_funded_AME_2020",
+                   "scottish_budget_2020"	,
+                   "fiscal_resource_2021",
+                   "non_cash_2021"	,
+                   "capital_2021",
+                   "financial_transactions_2021"	,
+                   "UK_funded_AME_2021",
+                   "scottish_budget_2021",
+                   "percentage_change",
+                   "what_it_buys",
+                   "change_explanation"
+)
+colnames(level4data) <- variable_names
+  
+
+#Now tidy up
+level4data <- level4data %>% select(-tmp)  #delete first column
+level4data <- level4data[!level4data$fiscal_resource_2020=="fiscal_resource_2020", ] # column names got mixed in with the data
+level4data <- level4data[!is.na(level4data$level4) | (!is.na(level4data$portfolio)),]
+# Check data types in columns. We see there are characters where we want numbers
+level4data %>% map_chr(class)
+
+#converting character columns to numeric where needed
+level4data <- level4data %>% mutate_at(vars(ends_with("2020")),as.numeric) %>% 
+  mutate_at(vars(ends_with("2021")),as.numeric) %>% 
+  mutate(percentage_change = as.numeric(percentage_change))
+
+level4data %>% map_chr(class)
 
 
 # Save to rds
